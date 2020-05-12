f5ff146965a4852681ef57fd96ae809ee8ed5d57
Nimzyow <n_soufiani@hotmail.com>
2020-05-12 22:55:32 +0100

    can post and get spots. also started creating tag model

--- routes/spots.js
+++ routes/spots.js
@@ -1,13 +1,65 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+
+const Spots = require("../models/Spots");
 
 // @route   GET api/spots
-// @Desc    Get Logged in user
+// @Desc    Get all spots
 // @access  Private
 
-router.get("/", (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
-  } catch (err) {}
+    const spots = await Spots.find();
+    res.json(spots);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+
+// @route   POST api/spots
+// @Desc    Add new spots
+// @access  Private
+
+router.post("/", auth, async (req, res) => {
+  const {
+    title,
+    description,
+    location,
+    url,
+    avg_cost,
+    latitude,
+    longitude,
+    summary,
+    address,
+    dress,
+    best_times,
+    advice,
+  } = req.body;
+
+  try {
+    const newSpot = new Spots({
+      title,
+      description,
+      location,
+      url,
+      avg_cost,
+      latitude,
+      longitude,
+      summary,
+      address,
+      dress,
+      best_times,
+      advice,
+    });
+
+    const spot = await newSpot.save();
+    res.json(spot);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
