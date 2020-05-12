a653fd73f8c26bc85406295bc28c5d6bb1481add
nimzyow <n_soufiani@hotmail.com>
2020-05-12 13:19:45 +0100

    initialised express validator in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,24 @@ const router = express.Router();
 const bcypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 // @route   GET api/auth
 // @Desc    Get Logged in user
 // @access  Private
 
-router.get("/", (req, res) => {
-  res.json({ msg: "get request in auth" });
-});
+router.get(
+  "/",
+  [
+    check("username", "Please add a username").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Please enter a password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    res.json({ msg: "get request in auth" });
+  }
+);
 
 // @route   POST api/auth
 // @dec     Auth user and get token
