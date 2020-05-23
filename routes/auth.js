b829a0956d1f7e3d011a8859f054c1d5ed0c8e66
Nimzyow <n_soufiani@hotmail.com>
2020-05-23 20:06:36 +0100

    Created passwordUtils and tokenUtils and refactored

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
 const { check, validationResult } = require("express-validator");
 const auth = require("../middleware/auth");
 
+const { compare } = require("../passwordUtils");
+const { generateToken } = require("../tokenUtils");
 const User = require("../models/User");
 
 // @route   GET api/auth
@@ -46,27 +45,18 @@ router.post(
       if (!user) {
         return res.status(401).json({ msg: "Invalid credentials" });
       }
-      const isMatch = await bcrypt.compare(password, user.password);
+      const isMatch = await compare(password, user.password);
 
       if (!isMatch) {
         return res.status(401).json({ msg: "email and password do not match" });
       }
-
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        },
-      );
+      try {
+        const token = await generateToken(user.id);
+        return res.status(200).json({ token });
+      } catch (err) {
+        console.error(err);
+        return res.statusCode(501).json({ msg: "Token generation failed" });
+      }
     } catch (err) {
       console.error(err.message);
       res.status(500).send("server error");
