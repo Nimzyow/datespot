f74a854cd5361c2d3b583ca5894d534415bd2afc
Nimzyow <n_soufiani@hotmail.com>
2020-05-23 11:24:49 +0100

    Created test to add 2 comments to a spot and passes test

--- routes/comments.js
+++ routes/comments.js
@@ -34,8 +34,7 @@ router.post("/:id/comments", auth, async (req, res) => {
       },
       { new: true },
     );
-
-    return res.status(200).json({ msg: "Comment added to spot" });
+    return res.status(200).json({ spot, msg: "Comment added to spot" });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ msg: "Server error" });
--- routes/routesTests/comments.test.js
+++ routes/routesTests/comments.test.js
@@ -26,4 +26,36 @@ describe("Comments route", () => {
     expect(response.statusCode).to.equal(200);
     expect(response.body.msg).to.equal("Comment added to spot");
   });
+  it("can add 2 comments", async () => {
+    const user = await createDBUser();
+    const newSpot = {
+      title: "a default title",
+      description: "a default description",
+      url: "www.google.com",
+      avgCost: "30",
+      summary: "A default summary",
+      address: "A default address",
+      advice: "A default advice",
+      comments: [{ userId: user.id, comment: "This is the first greatest comment in the world" }],
+    };
+
+    const token = await generateToken(user.id);
+    const spot = await createSpot(newSpot);
+
+    const userComment = {
+      comment: "This is the second greatest comment in the world",
+    };
+
+    const response = await request(app)
+      .post(`/api/spots/${spot.id}/comments`)
+      .set({
+        "Content-Type": "application/json",
+        "x-auth-token": token,
+      })
+      .send(userComment);
+
+    expect(response.statusCode).to.equal(200);
+    expect(response.body.msg).to.equal("Comment added to spot");
+    expect(response.body.spot.comments.length).to.equal(2);
+  });
 });
