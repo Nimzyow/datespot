a39f1a73c0ce5cd1049297485333ef31b5c26504
Nimzyow <n_soufiani@hotmail.com>
2020-05-26 00:13:19 +0100

    added scripts to prepare for deployment to heroku

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const app = require("./server");
 const db = require("./config/db");
+const path = require("path")
 
 db.connect();
 
-const PORT = 4000;
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("datespot-react/build"));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "datespot-react", "build", "index.html")));
+}
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
