5c27c2fad758158f7813f7b3f870fd0155923dce
Nimzyow <n_soufiani@hotmail.com>
2020-05-27 22:23:59 +0100

    Tags now uses Redux to load user

--- datespot-react/src/components/spot/SpotTags.js
+++ datespot-react/src/components/spot/SpotTags.js
@@ -1,27 +1,33 @@
 import React, { useContext, useEffect } from "react";
 import TagContext from "../../context/tag/TagContext";
-import AuthContext from "../../context/auth/AuthContext"
 import TagItem from "./TagItem";
 import { Spinner } from "react-bootstrap";
 
-const SpotTags = () => {
+import { connect } from "react-redux";
+
+import { loadUser } from "../../actions/authActions";
+
+const SpotTags = ({ loadUser }) => {
   const tagContext = useContext(TagContext);
-  const authContext = useContext(AuthContext)
   const { getTags, tags } = tagContext;
   useEffect(() => {
-    authContext.loadUser()
+    loadUser();
     getTags();
   }, []);
 
   return (
     <div style={{ fontSize: "17px", textAlign: "center" }}>
       {tags !== null ? (
-        tags.map((tag) => <TagItem key={tag._id} tag={tag.tag} tagId={tag._id} />)
+        tags.map((tag) => (
+          <TagItem key={tag._id} tag={tag.tag} tagId={tag._id} />
+        ))
       ) : (
-          <Spinner animation="border" variant="danger" />
-        )}
+        <Spinner animation="border" variant="danger" />
+      )}
     </div>
   );
 };
 
-export default SpotTags;
+const mapStateToProps = () => ({});
+
+export default connect(mapStateToProps, { loadUser })(SpotTags);
