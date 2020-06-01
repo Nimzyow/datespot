286a25e65246329fc4648610566c8b1ad0e18bc4
Nimzyow <n_soufiani@hotmail.com>
2020-06-01 22:11:16 +0100

    tested reducer for login and registration success

--- datespot-react/src/reducers/tests/authReducer.test.js
+++ datespot-react/src/reducers/tests/authReducer.test.js
@@ -12,4 +12,73 @@ describe("authReducer", () => {
 
     expect(authReducer(fakeState, unrelatedAction)).toEqual(fakeState);
   });
+  test("update state when user has successfully loaded", () => {
+    const action = {
+      type: types.USER_LOADED,
+      payload: "user information",
+    };
+
+    const expectedState = {
+      token: localStorage.getItem("token"),
+      isAuthenticated: true,
+      loading: false,
+      user: action.payload,
+      error: null,
+      spinner: false,
+      spinnerComment: "",
+    };
+
+    expect(authReducer(undefined, action)).toEqual(expectedState);
+  });
+  test("update state on authentication error", () => {
+    const action = {
+      type: types.AUTH_ERROR,
+      payload: "some error",
+    };
+    const expectedState = {
+      token: null,
+      isAuthenticated: false,
+      loading: false,
+      user: null,
+      error: action.payload,
+      spinner: false,
+      spinnerComment: "",
+    };
+
+    expect(authReducer(undefined, action)).toEqual(expectedState);
+  });
+  test("update state when user on successful registration", () => {
+    const action = {
+      type: types.REGISTER_SUCCESS,
+      payload: { token: "token" },
+    };
+    const expectedState = {
+      token: action.payload.token,
+      isAuthenticated: true,
+      loading: false,
+      user: null,
+      error: null,
+      spinner: false,
+      spinnerComment: "",
+    };
+
+    expect(authReducer(undefined, action)).toEqual(expectedState);
+  });
+  test("update state when user login is successful", () => {
+    const action = {
+      type: types.LOGIN_SUCCESS,
+      payload: { token: "token" },
+    };
+    const expectedState = {
+      token: action.payload.token,
+      isAuthenticated: true,
+      loading: false,
+      user: null,
+      error: null,
+      spinner: false,
+      spinnerComment: "",
+    };
+
+    expect(authReducer(undefined, action)).toEqual(expectedState);
+  });
 });
