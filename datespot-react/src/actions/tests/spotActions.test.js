c598a481beb7fa47255550fef621a6fca8cc317b
Nimzyow <n_soufiani@hotmail.com>
2020-06-03 21:07:24 +0100

    SPOTS_ERROR  action triggered on get spots failure

--- datespot-react/src/actions/tests/spotActions.test.js
+++ datespot-react/src/actions/tests/spotActions.test.js
@@ -4,7 +4,10 @@ import mockAxios from "axios";
 jest.mock("axios");
 
 describe("spotActions", () => {
-  test("getSpots dispatches to GET_SPOTS", async () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("getSpots function dispatches to GET_SPOTS", async () => {
     mockAxios.get.mockImplementationOnce(
       async () => await Promise.resolve({ data: "some spots" }),
     );
@@ -15,6 +18,21 @@ describe("spotActions", () => {
     expect(dispatch).toHaveBeenCalledWith({
       type: types.GET_SPOTS,
       payload: "some spots",
+    });
+    expect(mockAxios.get).toHaveBeenCalledWith("/api/spots");
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  });
+  test("getSpots function dispatches to SPOTS_ERROR on getSpots failure", async () => {
+    mockAxios.get.mockImplementationOnce(
+      async () => await Promise.reject({ err: "some error for spots" }),
+    );
+    const response = await actionType.getSpots();
+    const dispatch = jest.fn();
+    await response(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: types.SPOTS_ERROR,
+      payload: { err: "some error for spots" },
     });
     expect(mockAxios.get).toHaveBeenCalledWith("/api/spots");
     expect(mockAxios.get).toHaveBeenCalledTimes(1);
