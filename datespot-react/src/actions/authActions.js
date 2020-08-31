14f32c814c79cd22041a78b9a726e7164e3f0169
Nimzyow <n_soufiani@hotmail.com>
2020-08-31 12:07:14 +0100

    client side login user handled with grapql

--- datespot-react/src/actions/authActions.js
+++ datespot-react/src/actions/authActions.js
@@ -4,98 +4,107 @@ import setAuthToken from "../Utils/SetAuthToken";
 
 // Load User
 export const loadUser = () => {
-  return async (dispatch) => {
-    setAuthToken(localStorage.token);
+    return async (dispatch) => {
+        setAuthToken(localStorage.token);
 
-    try {
-      const res = await axios.get("/api/auth");
+        try {
+            const res = await axios.get("/api/auth");
 
-      dispatch({
-        type: Types.USER_LOADED,
-        payload: res.data,
-      });
-    } catch (err) {
-      console.error(err);
-      dispatch({ type: Types.AUTH_ERROR });
-    }
-  };
+            dispatch({
+                type: Types.USER_LOADED,
+                payload: res.data,
+            });
+        } catch (err) {
+            console.error(err);
+            dispatch({ type: Types.AUTH_ERROR });
+        }
+    };
 };
 
 // Register User
 export const register = (formData) => async (dispatch) => {
-  dispatch({ type: Types.SPINNER_SHOW });
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+    dispatch({ type: Types.SPINNER_SHOW });
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
 
-  try {
-    const res = await axios.post("/api/users", formData, config);
+    try {
+        const res = await axios.post("/api/users", formData, config);
 
-    dispatch({ type: Types.SPINNER_NOSHOW });
+        dispatch({ type: Types.SPINNER_NOSHOW });
 
-    dispatch({
-      type: Types.REGISTER_SUCCESS,
-      payload: res.data,
-    });
+        dispatch({
+            type: Types.REGISTER_SUCCESS,
+            payload: res.data,
+        });
 
-    loadUser();
-  } catch (err) {
-    dispatch({
-      type: Types.REGISTER_FAIL,
-      payload: err.response.data.msg,
-    });
-  }
+        loadUser();
+    } catch (err) {
+        dispatch({
+            type: Types.REGISTER_FAIL,
+            payload: err.response.data.msg,
+        });
+    }
 };
 
 // Login User
 export const login = (formData) => async (dispatch) => {
-  showSpinner();
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+    showSpinner();
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
 
-  dispatch({ type: Types.SPINNER_SHOW });
+    dispatch({ type: Types.SPINNER_SHOW });
 
-  try {
-    const res = await axios.post("/api/auth", formData, config);
+    const grapqlQuery = {
+        query:
+            "mutation ($email: String! $password: String!) { loginUser(email: $email password: $password) { token } }",
+        variables: { email: formData.email, password: formData.password },
+    };
 
-    dispatch({ type: Types.SPINNER_NOSHOW });
+    try {
+        const res = await axios.post(
+            "/graphql",
+            JSON.stringify(grapqlQuery),
+            config,
+        );
 
-    dispatch({
-      type: Types.LOGIN_SUCCESS,
-      payload: res.data,
-    });
+        dispatch({ type: Types.SPINNER_NOSHOW });
+        dispatch({
+            type: Types.LOGIN_SUCCESS,
+            payload: res.data.data.loginUser,
+        });
 
-    await loadUser();
-  } catch (err) {
-    dispatch({
-      type: Types.LOGIN_FAIL,
-      payload: "Invalid Credentials",
-    });
-    dispatch({ type: Types.SPINNER_NOSHOW });
-  }
+        await loadUser();
+    } catch (err) {
+        dispatch({
+            type: Types.LOGIN_FAIL,
+            payload: "Invalid Credentials",
+        });
+        dispatch({ type: Types.SPINNER_NOSHOW });
+    }
 };
 
 // Logout
 export const logOut = () => (dispatch) => {
-  dispatch({ type: Types.LOGOUT });
+    dispatch({ type: Types.LOGOUT });
 };
 
 //show spinner
 export const showSpinner = () => (dispatch) => {
-  dispatch({ type: Types.SPINNER_SHOW });
+    dispatch({ type: Types.SPINNER_SHOW });
 };
 
 //dont show spinner
 export const noShowSpinner = () => (dispatch) => {
-  dispatch({ type: Types.SPINNER_NOSHOW });
+    dispatch({ type: Types.SPINNER_NOSHOW });
 };
 
 // Clear Errors
 export const clearErrors = () => (dispatch) => {
-  dispatch({ type: Types.CLEAR_ERRORS });
+    dispatch({ type: Types.CLEAR_ERRORS });
 };
--- datespot-react/src/index.js
+++ datespot-react/src/index.js
@@ -8,10 +8,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import store from "./store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change
